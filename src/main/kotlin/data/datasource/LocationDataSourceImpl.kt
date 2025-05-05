dec307e521274dd8ccae21affaaec7ac1256e845
Thoraya <thoriahamdy99@gmail.com>
2025-05-06 00:33:17 +0300

    refactor the datasource to use the injected http client and the const variable location base url

--- src/main/kotlin/data/datasource/LocationDataSourceImpl.kt
+++ src/main/kotlin/data/datasource/LocationDataSourceImpl.kt
@@ -2,24 +2,18 @@ package org.example.data.datasource
 
 import io.ktor.client.*
 import io.ktor.client.call.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
-import io.ktor.serialization.kotlinx.json.*
-import kotlinx.serialization.json.Json
+import locationBaseUrl
 import org.example.data.dto.LocationDto
 import org.example.logic.exceptions.LoadingDataException
 
-class LocationDataSourceImpl : LocationDataSource {
+class LocationDataSourceImpl(
+    private val client: HttpClient
+) : LocationDataSource {
 
     override suspend fun getCurrentLocation(): LocationDto? {
         try {
-            val client = HttpClient(CIO) {
-                install(ContentNegotiation) {
-                    json(Json { ignoreUnknownKeys = true })
-                }
-            }
-            return client.get("http://ip-api.com/json").body<LocationDto>()
+            return client.get(locationBaseUrl).body<LocationDto?>()
 
         } catch (exception: Exception) {
             exception.printStackTrace()
