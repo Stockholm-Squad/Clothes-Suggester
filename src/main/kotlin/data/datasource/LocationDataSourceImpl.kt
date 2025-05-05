00259ba317c02c11ff74c42817a33aef569dd9eb
Thoraya <thoriahamdy99@gmail.com>
2025-05-05 20:46:24 +0300

    handle location data source to ignore un known keys

--- src/main/kotlin/data/datasource/LocationDataSourceImpl.kt
+++ src/main/kotlin/data/datasource/LocationDataSourceImpl.kt
@@ -3,7 +3,10 @@ package org.example.data.datasource
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
 import org.example.data.dto.LocationDto
 import org.example.logic.exceptions.LoadingDataException
 
@@ -11,11 +14,15 @@ class LocationDataSourceImpl : LocationDataSource {
 
     override suspend fun getCurrentLocation(): LocationDto? {
         try {
-            val client = HttpClient(CIO)
-            val url = "http://ip-api.com/json"
-            return client.get(url).body()
+            val client = HttpClient(CIO) {
+                install(ContentNegotiation) {
+                    json(Json { ignoreUnknownKeys = true })
+                }
+            }
+            return client.get("http://ip-api.com/json").body<LocationDto>()
 
         } catch (exception: Exception) {
+            exception.printStackTrace()
             throw LoadingDataException()
         }
     }
