8b4f2dd7da4106db39dd040b00eb28e56c1335e1
Thoraya <thoriahamdy99@gmail.com>
2025-05-05 19:20:49 +0300

    add needed dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,10 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     koinDependencies()
+
+    testingDependencies()
+    coroutinesDependencies()
+    ktorDependencies()
 }
 
 fun koinDependencies() {
@@ -26,4 +30,28 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(17)
+}
+
+fun testingDependencies() {
+    dependencies {
+        testImplementation("io.mockk:mockk:1.13.9")
+        testImplementation("com.google.truth:truth:1.4.2")
+        testImplementation("io.kotest:kotest-assertions-core:5.8.0")
+    }
+}
+
+fun ktorDependencies() {
+    dependencies {
+        implementation("ch.qos.logback:logback-classic:1.4.11")
+        implementation("io.ktor:ktor-client-core:2.3.5")
+        implementation("io.ktor:ktor-client-cio:2.3.5")
+        implementation("io.ktor:ktor-client-content-negotiation:2.3.5")
+        implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.5")
+    }
+}
+
+fun coroutinesDependencies() {
+    dependencies {
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    }
 }
