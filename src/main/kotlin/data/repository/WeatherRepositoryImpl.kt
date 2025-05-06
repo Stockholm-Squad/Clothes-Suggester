f2426859fa148a87ca9417b433c8cd93b41a13c7
Mona <monaayman584@gmail.com>
2025-05-06 18:08:14 +0300

    add implementation for testcases for WeatherRepositoryImpl and throw exception

--- src/main/kotlin/data/repository/WeatherRepositoryImpl.kt
+++ src/main/kotlin/data/repository/WeatherRepositoryImpl.kt
@@ -4,14 +4,20 @@ import logic.model.LocationModel
 import logic.model.WeatherModel
 import org.example.data.datasource.WeatherDataSource
 import org.example.data.mapper.toWeatherModelList
+import org.example.logic.exceptions.LoadingDataException
 import org.example.logic.repository.WeatherRepository
 
 class WeatherRepositoryImpl(
     private val weatherDataSource: WeatherDataSource,
 ) : WeatherRepository {
     override suspend fun getWeather(locationModel: LocationModel): List<WeatherModel> {
-        val weatherModel = weatherDataSource.getWeather(locationModel )
-        return weatherModel.toWeatherModelList()
+       try {
+           val weatherModel = weatherDataSource.getWeather(locationModel )
+           return weatherModel.toWeatherModelList()
+       }catch (exception: Exception){
+           exception.printStackTrace()
+           throw LoadingDataException()
+       }
     }
 
 
